98cde0106c679822d365d958538ef11a82db4631
ariel <ar.benlulu@duocuc.cl>
2025-12-17 19:16:50 -0300

    Update CompraResponse.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/carrito/carrito/carrito/src/main/java/com/example/carrito/dto/CompraResponse.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/carrito/carrito/carrito/src/main/java/com/example/carrito/dto/CompraResponse.java
@@ -1,10 +1,39 @@
 package com.example.carrito.dto;
 
-import lombok.Data;
+// BORRAMOS import lombok.Data;
 
-@Data
+// BORRAMOS @Data
 public class CompraResponse {
+    
     private int compraId;
     private String fecha;
     private String estado;
+
+    // ==========================================
+    //   GETTERS Y SETTERS (Manuales)
+    // ==========================================
+
+    public int getCompraId() {
+        return compraId;
+    }
+
+    public void setCompraId(int compraId) {
+        this.compraId = compraId;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
