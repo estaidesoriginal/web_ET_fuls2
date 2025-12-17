f52257612875b26ee0c7b6a7d36b1cb708fe4985
ariel <ar.benlulu@duocuc.cl>
2025-12-17 18:40:57 -0300

    Update ProductoController.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/catalogo/catalogo/catalogo/src/main/java/com/example/catalogo/controller/ProductoController.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/catalogo/catalogo/catalogo/src/main/java/com/example/catalogo/controller/ProductoController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(origins = "*")
+
 @RequiredArgsConstructor
 @CrossOrigin(
     origins = {"http://localhost:3000", "http://localhost:5173"},
