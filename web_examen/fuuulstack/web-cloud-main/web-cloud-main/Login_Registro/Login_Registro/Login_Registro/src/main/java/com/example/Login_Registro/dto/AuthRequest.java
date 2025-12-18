f4906c93f5cdb30e9e7c6a8ab709897d2dc38c28
ariel <ar.benlulu@duocuc.cl>
2025-12-18 03:37:57 -0300

    Update AuthRequest.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/dto/AuthRequest.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/dto/AuthRequest.java
@@ -1,7 +1,27 @@
 package com.example.Login_Registro.dto;
 
-public record AuthRequest(
-    String email,
-    String password
-) {}
+public class AuthRequest {
+
+    private String email;
+    private String password;
+
+    public AuthRequest() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
 
