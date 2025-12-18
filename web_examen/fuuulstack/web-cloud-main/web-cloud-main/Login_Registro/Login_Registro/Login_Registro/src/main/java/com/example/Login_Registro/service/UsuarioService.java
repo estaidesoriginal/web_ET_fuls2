38b70ce8f65737f32ba7dce37cb41d00083e427a
ariel <ar.benlulu@duocuc.cl>
2025-12-18 06:31:31 -0300

    Update UsuarioService.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/service/UsuarioService.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/service/UsuarioService.java
@@ -5,32 +5,19 @@ import com.example.Login_Registro.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public List<Usuario> obtenerTodos() {
-        return usuarioRepository.findAll();
-    }
-
     public Usuario login(String correo, String contrasena) {
-
-        Usuario usuario = usuarioRepository.findByCorreo(correo)
+        return usuarioRepository
+                .findByCorreoAndContrasena(correo, contrasena)
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-
-        if (!usuario.getContrasena().equals(contrasena)) {
-            throw new RuntimeException("Contraseña incorrecta");
-        }
-
-        return usuario;
     }
 
     public Usuario guardarUsuario(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 }
-
