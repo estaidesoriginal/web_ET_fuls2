bc8d4595dc2ab3ba0154a38570da631f5d747817
ariel <ar.benlulu@duocuc.cl>
2025-12-18 06:43:28 -0300

    Update UsuarioService.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/service/UsuarioService.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/service/UsuarioService.java
@@ -1,10 +1,12 @@
 package com.example.Login_Registro.service;
 
-import java.util.List;
 import com.example.Login_Registro.model.Usuario;
 import com.example.Login_Registro.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -12,22 +14,21 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-     public Usuario login(String correo, String contrasena) {
-        Usuario usuario = usuarioRepository.findByCorreo(correo)
-            .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-    
-        if (!usuario.getContrasena().equals(contrasena)) {
-            throw new RuntimeException("Contraseña incorrecta");
-        }
-    
-        return usuario;
-    }
-
-    public Usuario guardarUsuario(Usuario usuario) {
-        return usuarioRepository.save(usuario);
-    }
+    // PARA ADMIN
     public List<Usuario> obtenerTodos() {
         return usuarioRepository.findAll();
     }
 
+    // LOGIN
+    public Usuario login(String correo, String contrasena) {
+        return usuarioRepository
+                .findByCorreoAndContrasena(correo, contrasena)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+    }
+
+    // REGISTRO
+    public Usuario guardarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
+
