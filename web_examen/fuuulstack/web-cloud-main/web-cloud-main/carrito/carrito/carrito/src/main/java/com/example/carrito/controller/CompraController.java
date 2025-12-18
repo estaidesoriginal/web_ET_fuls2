c9673a6933b5f69cf5d7129ac5af55a6d1bb3fc3
ariel <ar.benlulu@duocuc.cl>
2025-12-17 22:00:37 -0300

    Update CompraController.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/carrito/carrito/carrito/src/main/java/com/example/carrito/controller/CompraController.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/carrito/carrito/carrito/src/main/java/com/example/carrito/controller/CompraController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(originPatterns = "*")
+@CrossOrigin(originPatterns = "*", allowedHeaders = "*", allowCredentials = "true")
 
 @RestController
 @RequestMapping("/api/carrito/compras")
