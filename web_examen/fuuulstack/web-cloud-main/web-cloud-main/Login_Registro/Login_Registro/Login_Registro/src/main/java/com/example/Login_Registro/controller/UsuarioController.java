4e1be8b57a8219cdadeaf1898409db92e8256db4
ariel <ar.benlulu@duocuc.cl>
2025-12-18 06:43:50 -0300

    Update UsuarioController.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/controller/UsuarioController.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/controller/UsuarioController.java
@@ -1,32 +1,36 @@
-package com.example.Login_Registro.controller; // ⚠️ AJUSTA A TU PAQUETE REAL
+package com.example.Login_Registro.controller;
 
-import com.example.Login_Registro.model.Usuario; // ⚠️ AJUSTA A TU PAQUETE REAL
-import com.example.Login_Registro.service.UsuarioService; // ⚠️ AJUSTA A TU PAQUETE REAL
+import com.example.Login_Registro.model.Usuario;
+import com.example.Login_Registro.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/usuarios") // ✅ Esto coincide con tu Frontend
-@CrossOrigin(origins = "*")      // ✅ Permite acceso desde cualquier lado (para probar)
+@RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
-    // GET: Para la tabla de Admin
+    // GET admin
     @GetMapping
     public List<Usuario> obtenerTodos() {
         return usuarioService.obtenerTodos();
     }
 
-    // POST: Para Login (Tu frontend llama a /api/usuarios/login)
+    // LOGIN
     @PostMapping("/login")
     public Usuario login(@RequestBody Usuario usuario) {
-        return usuarioService.login(usuario.getCorreo(), usuario.getContrasena());
+        return usuarioService.login(
+                usuario.getCorreo(),
+                usuario.getContrasena()
+        );
     }
-    
-    // POST: Para Registro
+
+    // REGISTRO
     @PostMapping("/register")
     public Usuario registrar(@RequestBody Usuario usuario) {
         return usuarioService.guardarUsuario(usuario);
