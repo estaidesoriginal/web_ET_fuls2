cdc2fbfabdc85769ffc382d96aadbd847ce31ba6
ariel <ar.benlulu@duocuc.cl>
2025-12-17 22:01:27 -0300

    Update ProductoController.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/catalogo/catalogo/catalogo/src/main/java/com/example/catalogo/controller/ProductoController.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/catalogo/catalogo/catalogo/src/main/java/com/example/catalogo/controller/ProductoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@CrossOrigin(originPatterns = "*")
+@CrossOrigin(originPatterns = "*", allowedHeaders = "*", allowCredentials = "true")
 
 @RequiredArgsConstructor
 
