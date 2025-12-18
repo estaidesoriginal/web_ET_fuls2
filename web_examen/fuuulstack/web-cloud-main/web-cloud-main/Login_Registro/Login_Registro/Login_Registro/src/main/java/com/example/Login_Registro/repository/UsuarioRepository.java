6a84a59fa07debe8ba1495ba6c61ada1597478a0
ariel <ar.benlulu@duocuc.cl>
2025-12-18 06:09:32 -0300

    Update UsuarioRepository.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/repository/UsuarioRepository.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/repository/UsuarioRepository.java
@@ -2,11 +2,10 @@ package com.example.Login_Registro.repository;
 
 import com.example.Login_Registro.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    Usuario findByCorreoAndContrasena(String correo, String contrasena);
+    Optional<Usuario> findByCorreo(String correo);
 }
-
