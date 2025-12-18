2bea8b8dabd4226812126fbe9d39d0893499f3f2
ariel <ar.benlulu@duocuc.cl>
2025-12-18 03:45:40 -0300

    Update Usuario.java

---
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/model/Usuario.java
@@ -0,0 +1,59 @@
+package com.example.Login_Registro.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    private String rol;
+
+    public Usuario() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
