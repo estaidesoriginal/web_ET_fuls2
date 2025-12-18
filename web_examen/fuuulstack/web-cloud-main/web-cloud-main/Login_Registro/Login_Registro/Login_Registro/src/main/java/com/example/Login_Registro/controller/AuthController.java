c5be35d2d0b50631fd7919c5d15d28feabcd7767
ariel <ar.benlulu@duocuc.cl>
2025-12-18 00:31:52 -0300

    Update AuthController.java

--- web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/controller/AuthController.java
+++ web_examen/fuuulstack/web-cloud-main/web-cloud-main/Login_Registro/Login_Registro/Login_Registro/src/main/java/com/example/Login_Registro/controller/AuthController.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Flux;
 @CrossOrigin(originPatterns = "*", allowedHeaders = "*", allowCredentials = "true")
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/usuarios")
 
 public class AuthController {
 
